70d16d7666d716b1d8f8b3a58bfe0df9255ab889
Vikas <xanderbilla@gmail.com>
2025-05-14 12:31:48 +0530

    Project initated

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,19 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Text, View } from "react-native";
+
 export default function HomeScreen() {
   return (
-    <View style={styles.container}>
-      <Text className='text-3xl text-red-500'>Hello Ankit</Text>
+    <View className="flex-1 bg-white items-center justify-center">
+      <Text className="text-3xl text-red-500">Welcome Xander Billa</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
